78c7bda01426d10eac4585955a1f151e76e2c8e9
thinkier <26728734+thinkier@users.noreply.github.com>
2021-02-06 00:07:42 +1100

    sponge incompatibility, maybe this will help?

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 unzip /tmp/po3.zip
 cp /app/mods/*.jar ./mods/
 
-java -Xmx8192M -Xms2048M -jar forge-1.12.2-14.23.5.2854.jar nogui
+java -Xmx6144M -Xms2048M -jar /app/forge-1.12.2-14.23.5.2838-universal.jar nogui
