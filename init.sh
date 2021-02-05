34020fba73cde20eb8d90008c4ebf37b9c74b97e
thinkier <26728734+thinkier@users.noreply.github.com>
2021-02-06 00:25:31 +1100

    oops

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ cp /app/mods/*.jar ./mods/
 
 java -jar /app/forge-1.12.2-14.23.5.2838-installer.jar --installServer
 
-java -Xmx6144M -Xms2048M-jar forge-1.12.2-14.23.5.2838-universal.jar
+java -Xmx6144M -Xms2048M -jar forge-1.12.2-14.23.5.2838-universal.jar
